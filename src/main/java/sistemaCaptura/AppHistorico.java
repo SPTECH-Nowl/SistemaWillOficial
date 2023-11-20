85aeba845e8865777407f86958eaa7f15bac5852
dantaswilljpgdev <dantaswill748@gmail.com>
2023-11-19 22:05:53 -0300

    version

--- src/main/java/sistemaCaptura/AppHistorico.java
+++ src/main/java/sistemaCaptura/AppHistorico.java
@@ -1,14 +1,25 @@
 package sistemaCaptura;
 
+import sistemaCaptura.conexao.Conexao;
+import sistemaCaptura.log.metodos.Logs;
+import sistemaCaptura.user.Usuario;
+import sistemaCaptura.user.Adiministrador;
+import sistemaCaptura.user.Professor;
+import sistemaCaptura.user.AdmNowl;
+import sistemaCaptura.user.Aluno;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import sistemaCaptura.conexao.Conexao;
-import sistemaCaptura.user.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class AppHistorico {
+
+    static Logs logs = new Logs();
+    static LocalDate dataAtual;
 
     public static void main(String[] args) {
         Conexao conexao = new Conexao();
@@ -17,6 +28,9 @@ public class AppHistorico {
 
         Scanner in = new Scanner(System.in);
         Integer escolha;
+        String motivoComeco= ":--SUCCESS: O Sistema iniciou normalmente";
+        logs.adicionarMotivo(motivoComeco);
+
 
         do {
             System.out.println("-".repeat(15));
@@ -54,6 +68,49 @@ public class AppHistorico {
         } while (escolha != 5);
     }
 
+
+    private static void cadastrarUsuario(JdbcTemplate con) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite o nome do usuário (mínimo de 3 caracteres):");
+        String nome = leitor.nextLine();
+        while (nome.length() < 3) {
+            System.out.println("O nome deve ter no mínimo 3 caracteres. Tente novamente:");
+            nome = leitor.nextLine();
+        }
+
+        System.out.println("Digite o email do usuário (deve conter '@'):");
+        String email = leitor.nextLine();
+        while (!email.contains("@")) {
+            System.out.println("O email deve conter o caractere '@'. Tente novamente:");
+            email = leitor.nextLine();
+        }
+
+        System.out.println("Digite a senha do usuário (mínimo de 8 caracteres):");
+        String senha = leitor.nextLine();
+        while (senha.length() < 8) {
+            System.out.println("A senha deve ter no mínimo 8 caracteres. Tente novamente:");
+            senha = leitor.nextLine();
+        }
+
+        System.out.println("Digite o ID da instituição (fkInstituicao):");
+        int fkInstituicao = leitor.nextInt();
+
+        System.out.println("Digite o ID do tipo de usuário (fkTipoUsuario) (deve ser 1, 2 ou 3):");
+        int fkTipoUsuario = leitor.nextInt();
+        while (fkTipoUsuario < 1 || fkTipoUsuario > 3) {
+            System.out.println("O ID do tipo de usuário deve ser 1, 2 ou 3. Tente novamente:");
+            fkTipoUsuario = leitor.nextInt();
+        }
+
+        // Outros campos de cadastro, se necessário
+
+        // Inserir o novo usuário no banco de dados com as colunas fkInstituicao e fkTipoUsuario
+        con.update("INSERT INTO usuario (nome, email, senha, fkInstituicao, fkTipoUsuario) VALUES (?, ?, ?, ?, ?)", nome, email, senha, fkInstituicao, fkTipoUsuario);
+
+        System.out.println("Usuário cadastrado com sucesso!");
+    }
+
     private static void fazerLogin(JdbcTemplate con, HistConsmRecurso histConsmRecurso, Scanner in) {
         Scanner leitor = new Scanner(System.in);
         Integer numeroMaquina = null;
@@ -77,20 +134,22 @@ public class AppHistorico {
                 new BeanPropertyRowMapper<>(Usuario.class), email, senha);
 
         if (usuarios.size() > 0) {
+            String motivo = ":--SUCCESS: O Sistema localizou " + usuarios.size() + " registro(s)  relacionado ao email ('" + email + "') e a senha ('" + senha + "')!";
+            logs.adicionarMotivo(motivo);
+
             if (usuarios.get(0).getFkTipoUsuario().equals(1)) {
                 usuario = new AdmNowl(usuarios.get(0));
-                System.out.println("Bem-vindo ADM Nowl " + usuario.getNome());
+                System.out.println("Bem vindo ADM Nowl" + usuario.getNome());
             } else if (usuarios.get(0).getFkTipoUsuario().equals(2)) {
                 usuario = new Adiministrador(usuarios.get(0));
-                System.out.println("Bem-vindo ADM " + usuario.getNome());
+                System.out.println("Bem vindo ADM " + usuario.getNome());
             } else if (usuarios.get(0).getFkTipoUsuario().equals(3)) {
                 usuario = new Professor(usuarios.get(0));
-                System.out.println("Bem-vindo Professor " + usuario.getNome());
+                System.out.println("Bem vindo Professor " + usuario.getNome());
             } else {
                 usuario = new Aluno(usuarios.get(0));
-                System.out.println("Bem-vindo Usuário " + usuario.getNome());
+                System.out.println("Bem vindo Usuario" + usuario.getNome());
             }
-
             Integer opcaoUsuario;
 
             do {
@@ -114,7 +173,7 @@ public class AppHistorico {
                     System.out.println("Opções de Professor");
                     System.out.println("P1 - listar máquinas disponíveis da sua instituição");
                     System.out.println("P2 - listar máquinas em uso da sua instituição");
-                    System.out.println("P3 - listar usuários da sua instituição");
+                    System.out.println("AP3 - listar usuários da sua instituição");
                 }
 
                 System.out.println("----------|| Opções do sistema ||----------");
@@ -125,61 +184,62 @@ public class AppHistorico {
                 opcaoUsuario = in.nextInt();
 
                 switch (opcaoUsuario) {
+
                     case 1:
-                        List<Maquina> maquinas = con.query("SELECT * FROM maquina WHERE emUso = 0",
-                                new BeanPropertyRowMapper<>(Maquina.class));
+                        List<Maquina> maquinas = con.query("SELECT * FROM maquina WHERE emUso = 0 AND fkInstituicao = ?",
+                                new BeanPropertyRowMapper<>(Maquina.class),usuario.getFkInstituicao());
+                        if (maquinas.size() > 0) {
+                            String motivoMaquina= ":--SUCCESS: O Sistema localizou "+maquinas.size()+" maquina(s) registrada(s)  relacionadas a intituição!";
+                            logs.adicionarMotivo(motivoMaquina);
 
-                        System.out.println("-".repeat(15));
-                        System.out.println("Escolha uma máquina disponível");
+                            System.out.println("-".repeat(15));
+                            System.out.println("Escolha uma máquina disponível");
 
-                        for (Maquina maquina : maquinas) {
-                            System.out.println("id: " + maquina.getIdMaquina());
-                            System.out.println("nome: " + maquina.getNome());
-                            System.out.println("Sistema Operacional: " + maquina.getSO());
-                            if (maquina.getDetalhes() != null) {
-                                System.out.println("Detalhes: " + maquina.getDetalhes());
+                            for (Maquina maquina : maquinas) {
+                                System.out.println("id: " + maquina.getIdMaquina());
+                                System.out.println("nome: " + maquina.getNome());
+                                System.out.println("Sistema Operacional: " + maquina.getSO());
+                                if (maquina.getDetalhes() != null) {
+                                    System.out.println("Detalhes: " + maquina.getDetalhes());
+                                }
                             }
+
+                            System.out.println("-".repeat(15));
+                            System.out.println("Digite o número da máquina");
+                            Integer numMaquina = in.nextInt();
+                            ativarMaquina(con, numMaquina, histConsmRecurso);
+                            numeroMaquina = numMaquina;
+
+                            List<Permissao> permissaos = con.query("SELECT * FROM permissao WHERE emUso = 1",
+                                    new BeanPropertyRowMapper<>(Permissao.class));
+                            System.out.println("-".repeat(15));
+                            for (Permissao permissao : permissaos) {
+                                System.out.println("Codigo aula: " + permissao.getNome());
+                            }
+                            System.out.println("-".repeat(15));
+                            System.out.println("Digite o código da aula");
+                            String codigoAula = leitor.nextLine();
+                            histConsmRecurso.mostrarHistorico(numeroMaquina, codigoAula);
+                            break;
+                        }else{
+                            String motivoMaquina= ":--ERROR: O Sistema não localizou registro de maquinas na sua intituição";
+                            logs.adicionarMotivo(motivoMaquina);
                         }
-
-                        System.out.println("-".repeat(15));
-                        System.out.println("Digite o número da máquina");
-                        Integer numMaquina = in.nextInt();
-                        ativarMaquina(con, numMaquina, histConsmRecurso);
-                        numeroMaquina = numMaquina;
-                        System.out.println("Digite o código da aula");
-                        leitor.nextLine(); // Consumir a quebra de linha pendente
-                        String codigoAula = leitor.nextLine();
-                        histConsmRecurso.mostrarHistorico(numeroMaquina, codigoAula);
-                        break;
-
                     case 2:
                         desativarMaquina(con, numeroMaquina);
                         exibirMensagemDespedida();
-                        histConsmRecurso.fecharSistema();
-                        return; // Isso encerrará o programa
+                        histConsmRecurso.fecharSistema();// Isso encerrará o programa
+                        return;
                     default:
                         System.out.println("Opção inválida");
                 }
             } while (opcaoUsuario != 2);
         } else {
             System.out.println("Dados de login inválidos");
+
+            String motivo = ":--ERROR: O Sistema não localizou nenhum dado relacionado ao email ('" + email + "') e a senha ('" + senha + "')!";
+            logs.adicionarMotivo(motivo);
         }
-    }
-
-    private static void ativarMaquina(JdbcTemplate con, Integer maquinaId, HistConsmRecurso histConsmRecurso) {
-        Maquina maquina = con.queryForObject("SELECT * FROM maquina WHERE idMaquina = ? AND emUso = 0",
-                new BeanPropertyRowMapper<>(Maquina.class), maquinaId);
-
-        if (maquina != null) {
-            con.update("UPDATE maquina SET emUso = 1 WHERE idMaquina = ?", maquinaId);
-            System.out.println("Máquina ativada com sucesso: " + maquina.getNome());
-        } else {
-            System.out.println("Máquina não disponível ou inválida.");
-        }
-    }
-
-    private static void desativarMaquina(JdbcTemplate con, Integer maquinaId) {
-        con.update("UPDATE maquina SET emUso = 0 WHERE idMaquina = ?", maquinaId);
     }
 
     private static void visualizarEditarPerfil(JdbcTemplate con) {
@@ -262,13 +322,30 @@ public class AppHistorico {
     }
 
 
+
+    private static void ativarMaquina(JdbcTemplate con, Integer maquinaId, HistConsmRecurso histConsmRecurso) {
+        Maquina maquina = con.queryForObject("SELECT * FROM maquina WHERE idMaquina = ? AND emUso = 0",
+                new BeanPropertyRowMapper<>(Maquina.class), maquinaId);
+
+        if (maquina != null) {
+            con.update("UPDATE maquina SET emUso = 1 WHERE idMaquina = ?", maquinaId);
+            System.out.println("Máquina ativada com sucesso: " + maquina.getNome());
+        } else {
+            System.out.println("Máquina não disponível ou inválida.");
+        }
+    }
+
+    private static void desativarMaquina(JdbcTemplate con, Integer maquinaId) {
+        con.update("UPDATE maquina SET emUso = 0 WHERE idMaquina = ?", maquinaId);
+    }
+
     private static void exibirMensagemDespedida() {
         System.out.println("+---------------------------------------------------------------+");
-        System.out.println("|                      \u001B[38;2;109;73;157mMAGISTER\u001B[0m                         |");
-        System.out.println("|   \u001B[38;2;109;73;157mObrigado por usar o nosso sistema\u001B[0m               |"); // Cor personalizada
-        System.out.println("|   \u001B[38;2;109;73;157mTenha um ótimo dia e uma vida incrível\u001B[0m      |"); // Cor personalizada
-        System.out.println("|                                                                   |");
-        System.out.println("|                        \u001B[38;2;109;73;157m#6D499D\u001B[0m                     |"); // Cor personalizada
+        System.out.println("|                            MAGISTER                           |");
+        System.out.println("|             Obrigado por usar o nosso sistema                 |");
+        System.out.println("|          Tenha um ótimo dia e uma vida incrível               |");
+        System.out.println("|                                                               |");
+        System.out.println("|                        #6D499D                                |");
         System.out.println("+---------------------------------------------------------------+");
     }
 
@@ -300,6 +377,19 @@ public class AppHistorico {
         cadastrarHardwareEComponente(con, idMaquina);
 
         System.out.println("Máquina cadastrada com sucesso!");
+    }
+
+    private static Integer cadastrarTipoHardware(JdbcTemplate con) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite o nome do tipo de hardware (ex: CPU, GPU, Memória):");
+        String tipoHardware = leitor.nextLine();
+
+        // Cadastrar o tipo de hardware no banco de dados
+        con.update("INSERT INTO tipoHardware (tipo) VALUES (?)", tipoHardware);
+
+        // Recuperar o ID do tipo de hardware recém-cadastrado
+        return con.queryForObject("SELECT MAX(idTipoHardware) FROM tipoHardware", Integer.class);
     }
 
     private static void cadastrarHardwareEComponente(JdbcTemplate con, Integer idMaquina) {
@@ -345,19 +435,4 @@ public class AppHistorico {
 
         System.out.println("Hardware e componente cadastrados com sucesso!");
     }
-
-
-    private static Integer cadastrarTipoHardware(JdbcTemplate con) {
-        Scanner leitor = new Scanner(System.in);
-
-        System.out.println("Digite o nome do tipo de hardware (ex: CPU, GPU, Memória):");
-        String tipoHardware = leitor.nextLine();
-
-        // Cadastrar o tipo de hardware no banco de dados
-        con.update("INSERT INTO tipoHardware (tipo) VALUES (?)", tipoHardware);
-
-        // Recuperar o ID do tipo de hardware recém-cadastrado
-        return con.queryForObject("SELECT MAX(idTipoHardware) FROM tipoHardware", Integer.class);
-    }
-
 }
--- src/main/java/sistemaCaptura/log/metodos/Logs.java
+++ src/main/java/sistemaCaptura/log/metodos/Logs.java
@@ -8,7 +8,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
@@ -17,7 +20,7 @@ public class Logs {
     private static final int LIMITE_CPU = 60;  // Defina o limite máximo de CPU conforme necessário
     private static final int LIMITE_RAM = 60;  // Defina o limite máximo de RAM conforme necessário
 
-    public static void gerarLog(Maquina maquina, Long consumoCpu, Long consumoRam, Long consumoDisco) {
+    public static void gerarLog(Maquina maquina, Long consumoCpu, double consumoRam, double consumoDisco) {
         LocalDate dataAtual = LocalDate.now();
         DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyyMMdd", Locale.ENGLISH);
         String nomeArquivo = dateFormat.format(dataAtual) + "_log.txt";
@@ -30,7 +33,7 @@ public class Logs {
         }
     }
 
-    private static void adicionarMensagens(String caminhoCompleto, LocalDate dataAtual, Maquina maquina, Long consumoCpu, Long consumoRam, Long consumoDisco) {
+    private static void adicionarMensagens(String caminhoCompleto, LocalDate dataAtual, Maquina maquina, Long consumoCpu, double consumoRam, double consumoDisco) {
         try (BufferedWriter writer = Files.newBufferedWriter(Path.of(caminhoCompleto), StandardOpenOption.APPEND)) {
 
             String mensagemSuporte = "Suporte foi solicitado para arrumar  a maquina (" + maquina.getNome() + ").";
@@ -38,7 +41,7 @@ public class Logs {
             // Adicionar mensagem relacionada ao consumo máximo de CPU e RAM
             String mensagemConsumo = String.format("O consumo de CPU estourou o máximo sugerido (%d%%). O consumo de RAM atingiu o máximo sugerido (%d%%) de acordo com o nome da máquina.%n", LIMITE_CPU, LIMITE_RAM);
 
-            String dados = String.format("Data/Hora: %s%nNome da Máquina: %s%nConsumo CPU: %d%%%nConsumo RAM: %d bytes%nConsumo Disco: %d GB%nMensagem para Suporte: %s%n%s%n",
+            String dados = String.format("Data/Hora: %s%nNome da Máquina: %s%nConsumo CPU: %d%%%nConsumo RAM: %.2f bytes%nConsumo Disco: %.2f GB%nMensagem para Suporte: %s%n%s%n",
                     dataAtual, maquina.getNome(), consumoCpu, consumoRam, consumoDisco, mensagemSuporte, mensagemConsumo);
 
             writer.write(dados);
@@ -50,15 +53,43 @@ public class Logs {
 
     private static void criarNovoArquivo(String caminhoCompleto, LocalDate dataAtual) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(caminhoCompleto))) {
-//            System.out.println("Novo log gerado com sucesso em: " + caminhoCompleto);
+            System.out.println("log gerado com sucesso em: " + caminhoCompleto);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void adicionarMotivo(String caminhoCompleto,Maquina maquina){
-        String mensagemSuporte = "Suporte foi solicitado para arrumar  a maquina (" + maquina.getNome() + ").";
+    public static void adicionarMotivo(String mensagem){
 
+        LocalDate dataAtual = LocalDate.now();
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyyMMdd", Locale.ENGLISH);
+        String nomeArquivo = dateFormat.format(dataAtual) + "_log.txt";
+        String caminhoCompleto = CAMINHO_ARQUIVO + nomeArquivo;
+
+
+        if (Files.exists(Path.of(caminhoCompleto))) {
+
+            LocalDateTime dataAtualLogs = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
+
+
+            // Formata a data de acordo com o formato especificado
+            String dataFormatadaLog = formatter.format(dataAtualLogs);
+
+            String mesagemLog =dataFormatadaLog+mensagem;
+
+            salvarMensagem(caminhoCompleto,mesagemLog);
+        } else {
+            criarNovoArquivo(caminhoCompleto, dataAtual);
+        }
+    }
+
+    private static void salvarMensagem(String caminhoCompleto,String mensagem ){
+        try (BufferedWriter writer = Files.newBufferedWriter(Path.of(caminhoCompleto), StandardOpenOption.APPEND)) {
+            writer.write(mensagem+"\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
