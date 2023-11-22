6ddf4ba1c353f8b550b1c82869419d24d542d3d2
dantaswilljpgdev <dantaswill748@gmail.com>
2023-11-22 12:21:19 -0300

    jar mysql e sql

--- src/main/java/sistemaCaptura/AppHistorico.java
+++ src/main/java/sistemaCaptura/AppHistorico.java
@@ -1,7 +1,6 @@
 package sistemaCaptura;
 
 import sistemaCaptura.conexao.Conexao;
-import sistemaCaptura.conexao.ConexãoSql;
 import sistemaCaptura.log.metodos.Logs;
 import sistemaCaptura.user.Usuario;
 import sistemaCaptura.user.Adiministrador;
@@ -21,27 +20,25 @@ public class AppHistorico {
 
     static Logs logs = new Logs();
     static LocalDate dataAtual;
+    static Conexao conexao = new Conexao();
 
     public static void main(String[] args) {
-        ConexãoSql conexao = new ConexãoSql();
-        JdbcTemplate con = conexao.getConnection();
+        JdbcTemplate con = conexao.getConexaoDoBanco();
         HistConsmRecurso histConsmRecurso = new HistConsmRecurso();
 
         Scanner in = new Scanner(System.in);
         Integer escolha;
-        String motivoComeco= ":--SUCCESS: O Sistema iniciou normalmente";
+        String motivoComeco = ":--SUCCESS: O Sistema iniciou normalmente";
         logs.adicionarMotivo(motivoComeco);
-
-
         do {
             System.out.println("-".repeat(15));
-            System.out.println("Bem-vindo ao sistema Nowl-Will Dantas");
+            System.out.println("Bem-vindo ao sistema Nowl");
             System.out.println("Escolha uma das opções abaixo");
             System.out.println("1 - Fazer login");
             System.out.println("2 - Sair");
-            System.out.println("3 - Visualizar e Editar Perfil");
-            System.out.println("4 - Sugestões");
-            System.out.println("5 - Cadastrar maquina");
+            System.out.println("3 - Cadastrar máquina");
+            System.out.println("4 - Visualizar e Editar Perfil");
+            System.out.println("5 - Enviar sugestão do projeto");
             System.out.println("-".repeat(15));
 
             escolha = in.nextInt();
@@ -55,20 +52,19 @@ public class AppHistorico {
                     histConsmRecurso.fecharSistema();
                     break;
                 case 3:
-                    visualizarEditarPerfil(con);
+                    cadastrarMaquina(con);
                     break;
                 case 4:
-                    enviarSugestao(con, in);
+                    visualizarEditarPerfil(con);
                     break;
                 case 5:
-                    cadastrarMaquina(con);
+                    enviarSugestao(con, in);
                     break;
                 default:
                     System.out.println("Opção inválida");
             }
-        } while (escolha != 5);
+        } while (escolha != 2);
     }
-
 
     private static void cadastrarUsuario(JdbcTemplate con) {
         Scanner leitor = new Scanner(System.in);
@@ -107,7 +103,12 @@ public class AppHistorico {
         // Outros campos de cadastro, se necessário
 
         // Inserir o novo usuário no banco de dados com as colunas fkInstituicao e fkTipoUsuario
-        con.update("INSERT INTO usuario (nome, email, senha, fkInstituicao, fkTipoUsuario) VALUES (?, ?, ?, ?, ?)", nome, email, senha, fkInstituicao, fkTipoUsuario);
+        if (conexao.getDev()) {
+            con.update("INSERT INTO usuario (nome, email, senha, fkInstituicao, fkTipoUsuario) VALUES (?, ?, ?, ?, ?)", nome, email, senha, fkInstituicao, fkTipoUsuario);
+        } else {
+            con.update("INSERT INTO usuario (nome, email, senha, fkInstituicao, fkTipoUsuario) VALUES (?, ?, ?, ?, ?)", nome, email, senha, fkInstituicao, fkTipoUsuario);
+        }
+
 
         System.out.println("Usuário cadastrado com sucesso!");
     }
@@ -130,23 +131,27 @@ public class AppHistorico {
             System.out.println("A senha deve ter no mínimo 8 caracteres. Tente novamente:");
             senha = leitor.nextLine();
         }
-
-        List<Usuario> usuarios = con.query("SELECT * FROM usuario WHERE email = ? AND senha = ?",
-                new BeanPropertyRowMapper<>(Usuario.class), email, senha);
-
+        List<Usuario> usuarios;
+        if (conexao.getDev()) {
+            usuarios = con.query("SELECT * FROM usuario WHERE email = ? AND senha = ?",
+                    new BeanPropertyRowMapper<>(Usuario.class), email, senha);
+        } else {
+            usuarios = con.query("SELECT * FROM usuario WHERE email = ? AND senha = ?",
+                    new BeanPropertyRowMapper<>(Usuario.class), email, senha);
+        }
         if (usuarios.size() > 0) {
             String motivo = ":--SUCCESS: O Sistema localizou " + usuarios.size() + " registro(s)  relacionado ao email ('" + email + "') e a senha ('" + senha + "')!";
             logs.adicionarMotivo(motivo);
 
             if (usuarios.get(0).getFkTipoUsuario().equals(1)) {
                 usuario = new AdmNowl(usuarios.get(0));
-                System.out.println("Bem vindo ADM Nowl" + usuario.getNome());
+                System.out.println("Bem vindo ADM Nowl-" + usuario.getNome());
             } else if (usuarios.get(0).getFkTipoUsuario().equals(2)) {
                 usuario = new Adiministrador(usuarios.get(0));
-                System.out.println("Bem vindo ADM " + usuario.getNome());
+                System.out.println("Bem vindo ADM- " + usuario.getNome());
             } else if (usuarios.get(0).getFkTipoUsuario().equals(3)) {
                 usuario = new Professor(usuarios.get(0));
-                System.out.println("Bem vindo Professor " + usuario.getNome());
+                System.out.println("Bem vindo Professor- " + usuario.getNome());
             } else {
                 usuario = new Aluno(usuarios.get(0));
                 System.out.println("Bem vindo Usuario" + usuario.getNome());
@@ -156,26 +161,26 @@ public class AppHistorico {
             do {
                 System.out.println("-".repeat(15));
 
-                if (usuario instanceof AdmNowl) {
-                    System.out.println("Opções de ADM Nowl");
-                    System.out.println("N1 - listar máquinas disponíveis de alguma empresa");
-                    System.out.println("N2 - listar usuários de alguma empresa");
-                    System.out.println("N3 - Cadastrar usuário ");
-                }
+//                if (usuario instanceof AdmNowl) {
+//                    System.out.println("Opções de ADM Nowl");
+//                    System.out.println("N1 - listar máquinas disponíveis de alguma empresa");
+//                    System.out.println("N2 - listar usuários de alguma empresa");
+//                    System.out.println("N3 - Cadastrar usuário ");
+//                }
 
-                if (usuario instanceof Adiministrador) {
-                    System.out.println("Opções de Administrador");
-                    System.out.println("A1 - listar máquinas disponíveis da sua instituição");
-                    System.out.println("A2 - listar máquinas em uso da sua instituição");
-                    System.out.println("A3 - listar usuários da sua instituição");
-                    System.out.println("A4 - Cadastrar usuário ");
-                }
-                if (usuario instanceof Professor) {
-                    System.out.println("Opções de Professor");
-                    System.out.println("P1 - listar máquinas disponíveis da sua instituição");
-                    System.out.println("P2 - listar máquinas em uso da sua instituição");
-                    System.out.println("AP3 - listar usuários da sua instituição");
-                }
+//                if (usuario instanceof Adiministrador) {
+//                    System.out.println("Opções de Administrador");
+//                    System.out.println("A1 - listar máquinas disponíveis da sua instituição");
+//                    System.out.println("A2 - listar máquinas em uso da sua instituição");
+//                    System.out.println("A3 - listar usuários da sua instituição");
+//                    System.out.println("A4 - Cadastrar usuário ");
+//                }
+//                if (usuario instanceof Professor) {
+//                    System.out.println("Opções de Professor");
+//                    System.out.println("P1 - listar máquinas disponíveis da sua instituição");
+//                    System.out.println("P2 - listar máquinas em uso da sua instituição");
+//                    System.out.println("AP3 - listar usuários da sua instituição");
+//                }
 
                 System.out.println("----------|| Opções do sistema ||----------");
                 System.out.println("1 - Ativar máquina");
@@ -187,10 +192,16 @@ public class AppHistorico {
                 switch (opcaoUsuario) {
 
                     case 1:
-                        List<Maquina> maquinas = con.query("SELECT * FROM maquina WHERE emUso = 0 AND fkInstituicao = ?",
-                                new BeanPropertyRowMapper<>(Maquina.class),usuario.getFkInstituicao());
+                        List<Maquina> maquinas;
+                        if (conexao.getDev()) {
+                            maquinas = con.query("SELECT * FROM maquina WHERE emUso = 0 AND fkInstituicao = ?",
+                                    new BeanPropertyRowMapper<>(Maquina.class), usuario.getFkInstituicao());
+                        } else {
+                            maquinas = con.query("SELECT * FROM maquina WHERE emUso = 0 AND fkInstituicao = ?",
+                                    new BeanPropertyRowMapper<>(Maquina.class), usuario.getFkInstituicao());
+                        }
                         if (maquinas.size() > 0) {
-                            String motivoMaquina= ":--SUCCESS: O Sistema localizou "+maquinas.size()+" maquina(s) registrada(s)  relacionadas a intituição!";
+                            String motivoMaquina = ":--SUCCESS: O Sistema localizou " + maquinas.size() + " maquina(s) registrada(s)  relacionadas a intituição!";
                             logs.adicionarMotivo(motivoMaquina);
 
                             System.out.println("-".repeat(15));
@@ -210,9 +221,14 @@ public class AppHistorico {
                             Integer numMaquina = in.nextInt();
                             ativarMaquina(con, numMaquina, histConsmRecurso);
                             numeroMaquina = numMaquina;
-
-                            List<Permissao> permissaos = con.query("SELECT * FROM permissao WHERE emUso = 1",
-                                    new BeanPropertyRowMapper<>(Permissao.class));
+                            List<Permissao> permissaos;
+                            if (conexao.getDev()) {
+                                permissaos = con.query("SELECT * FROM permissao WHERE emUso = 1",
+                                        new BeanPropertyRowMapper<>(Permissao.class));
+                            } else {
+                                permissaos = con.query("SELECT * FROM permissao WHERE emUso = 1",
+                                        new BeanPropertyRowMapper<>(Permissao.class));
+                            }
                             System.out.println("-".repeat(15));
                             for (Permissao permissao : permissaos) {
                                 System.out.println("Codigo aula: " + permissao.getNome());
@@ -222,8 +238,8 @@ public class AppHistorico {
                             String codigoAula = leitor.nextLine();
                             histConsmRecurso.mostrarHistorico(numeroMaquina, codigoAula);
                             break;
-                        }else{
-                            String motivoMaquina= ":--ERROR: O Sistema não localizou registro de maquinas na sua intituição";
+                        } else {
+                            String motivoMaquina = ":--ERROR: O Sistema não localizou registro de maquinas na sua intituição";
                             logs.adicionarMotivo(motivoMaquina);
                         }
                     case 2:
@@ -240,6 +256,24 @@ public class AppHistorico {
 
             String motivo = ":--ERROR: O Sistema não localizou nenhum dado relacionado ao email ('" + email + "') e a senha ('" + senha + "')!";
             logs.adicionarMotivo(motivo);
+        }
+    }
+
+    private static void ativarMaquina(JdbcTemplate con, Integer maquinaId, HistConsmRecurso histConsmRecurso) {
+        Maquina maquina = con.queryForObject("SELECT * FROM maquina WHERE idMaquina = ? AND emUso = 0",
+                new BeanPropertyRowMapper<>(Maquina.class), maquinaId);
+
+        if (maquina != null) {
+            if (conexao.getDev()) {
+
+                con.update("UPDATE maquina SET emUso = 1 WHERE idMaquina = ?", maquinaId);
+                System.out.println("Máquina ativada com sucesso: " + maquina.getNome());
+            } else {
+                con.update("UPDATE maquina SET emUso = 1 WHERE idMaquina = ?", maquinaId);
+                System.out.println("Máquina ativada com sucesso: " + maquina.getNome());
+            }
+        } else {
+            System.out.println("Máquina não disponível ou inválida.");
         }
     }
 
@@ -322,22 +356,13 @@ public class AppHistorico {
         System.out.println("Sugestão enviada com sucesso!");
     }
 
-
-
-    private static void ativarMaquina(JdbcTemplate con, Integer maquinaId, HistConsmRecurso histConsmRecurso) {
-        Maquina maquina = con.queryForObject("SELECT * FROM maquina WHERE idMaquina = ? AND emUso = 0",
-                new BeanPropertyRowMapper<>(Maquina.class), maquinaId);
-
-        if (maquina != null) {
-            con.update("UPDATE maquina SET emUso = 1 WHERE idMaquina = ?", maquinaId);
-            System.out.println("Máquina ativada com sucesso: " + maquina.getNome());
-        } else {
-            System.out.println("Máquina não disponível ou inválida.");
-        }
-    }
-
     private static void desativarMaquina(JdbcTemplate con, Integer maquinaId) {
-        con.update("UPDATE maquina SET emUso = 0 WHERE idMaquina = ?", maquinaId);
+        if (conexao.getDev()) {
+            con.update("UPDATE maquina SET emUso = 0 WHERE idMaquina = ?", maquinaId);
+        } else {
+            con.update("UPDATE maquina SET emUso = 0 WHERE idMaquina = ?", maquinaId);
+
+        }
     }
 
     private static void exibirMensagemDespedida() {
@@ -366,11 +391,23 @@ public class AppHistorico {
         int fkInstituicao = leitor.nextInt();
 
         // Cadastrar a máquina no banco de dados
-        con.update("INSERT INTO maquina (nome, SO, emUso, fkInstituicao) VALUES (?, ?, ?, ?)",
-                nomeMaquina, sistemaOperacional, emUso, fkInstituicao);
+        if (conexao.getDev()) {
 
+            con.update("INSERT INTO maquina (nome, SO, emUso, fkInstituicao) VALUES (?, ?, ?, ?)", nomeMaquina, sistemaOperacional, emUso, fkInstituicao);
+        } else {
+            con.update("INSERT INTO maquina (nome, SO, emUso, fkInstituicao) VALUES (?, ?, ?, ?)", nomeMaquina, sistemaOperacional, emUso, fkInstituicao);
+
+        }
+
+        Integer idMaquina;
+        if (conexao.getDev()) {
+            idMaquina = con.queryForObject("SELECT MAX(idMaquina) FROM maquina", Integer.class);
+
+        } else {
+            idMaquina = con.queryForObject("SELECT MAX(idMaquina) FROM maquina", Integer.class);
+
+        }
         // Recuperar o ID da máquina recém-cadastrada
-        Integer idMaquina = con.queryForObject("SELECT MAX(idMaquina) FROM maquina", Integer.class);
 
         // Cadastrar hardware e componente para a máquina
         cadastrarHardwareEComponente(con, idMaquina);
@@ -387,10 +424,20 @@ public class AppHistorico {
         String tipoHardware = leitor.nextLine();
 
         // Cadastrar o tipo de hardware no banco de dados
-        con.update("INSERT INTO tipoHardware (tipo) VALUES (?)", tipoHardware);
+        if (conexao.getDev()) {
 
+            con.update("INSERT INTO tipoHardware (tipo) VALUES (?)", tipoHardware);
+        } else {
+            con.update("INSERT INTO tipoHardware (tipo) VALUES (?)", tipoHardware);
+
+        }
         // Recuperar o ID do tipo de hardware recém-cadastrado
-        return con.queryForObject("SELECT MAX(idTipoHardware) FROM tipoHardware", Integer.class);
+        if (conexao.getDev()) {
+
+            return con.queryForObject("SELECT MAX(idTipoHardware) FROM tipoHardware", Integer.class);
+        } else {
+            return con.queryForObject("SELECT MAX(idTipoHardware) FROM tipoHardware", Integer.class);
+        }
     }
 
     private static void cadastrarHardwareEComponente(JdbcTemplate con, Integer idMaquina) {
@@ -415,24 +462,49 @@ public class AppHistorico {
         String tipoHardware = leitor.nextLine();
 
         // Cadastrar o tipo de hardware no banco de dados
-        con.update("INSERT INTO tipoHardware (tipo) VALUES (?)", tipoHardware);
+        if (conexao.getDev()) {
+            con.update("INSERT INTO tipoHardware (tipo) VALUES (?)", tipoHardware);
+        } else {
+            con.update("INSERT INTO tipoHardware (tipo) VALUES (?)", tipoHardware);
 
+        }
         // Recuperar o ID do tipo de hardware recém-cadastrado
-        Integer fkTipoHardware = con.queryForObject("SELECT MAX(idTipoHardware) FROM tipoHardware", Integer.class);
+        Integer fkTipoHardware;
+        if (conexao.getDev()) {
+            fkTipoHardware = con.queryForObject("SELECT MAX(idTipoHardware) FROM tipoHardware", Integer.class);
+
+        } else {
+            fkTipoHardware = con.queryForObject("SELECT MAX(idTipoHardware) FROM tipoHardware", Integer.class);
+
+        }
 
         // Cadastrar o hardware no banco de dados, incluindo fkTipoHardware
-        con.update("INSERT INTO hardware (fabricante, modelo, capacidade, especificidade, fkTipoHardware) VALUES (?, ?, ?, ?, ?)", fabricante, modelo, capacidade, especificidade, fkTipoHardware);
+        if (conexao.getDev()) {
 
+            con.update("INSERT INTO hardware (fabricante, modelo, capacidade, especificidade, fkTipoHardware) VALUES (?, ?, ?, ?, ?)", fabricante, modelo, capacidade, especificidade, fkTipoHardware);
+        } else {
+            con.update("INSERT INTO hardware (fabricante, modelo, capacidade, especificidade, fkTipoHardware) VALUES (?, ?, ?, ?, ?)", fabricante, modelo, capacidade, especificidade, fkTipoHardware);
+        }
         // Recuperar o ID do hardware recém-cadastrado
-        Integer idHardware = con.queryForObject("SELECT MAX(idHardware) FROM hardware", Integer.class);
-
+        Integer idHardware;
+        if (conexao.getDev()) {
+            idHardware = con.queryForObject("SELECT MAX(idHardware) FROM hardware", Integer.class);
+        } else {
+            idHardware = con.queryForObject("SELECT MAX(idHardware) FROM hardware", Integer.class);
+        }
         // Cadastrar componente
         System.out.println("Digite a porcentagem máxima para o componente (deixe em branco para usar o valor padrão):");
         String inputMax = leitor.nextLine();
         Integer max = (inputMax.isEmpty()) ? null : Integer.parseInt(inputMax);
 
         // Cadastrar o componente no banco de dados, incluindo fkTipoHardware
-        con.update("INSERT INTO componente (max, fkMaquina, fkHardware) VALUES (?, ?, ?)", max, idMaquina, idHardware);
+        if (conexao.getDev()) {
+
+            con.update("INSERT INTO componente (max, fkMaquina, fkHardware) VALUES (?, ?, ?)", max, idMaquina, idHardware);
+        }else{
+            con.update("INSERT INTO componente (max, fkMaquina, fkHardware) VALUES (?, ?, ?)", max, idMaquina, idHardware);
+
+        }
 
         System.out.println("Hardware e componente cadastrados com sucesso!");
     }
